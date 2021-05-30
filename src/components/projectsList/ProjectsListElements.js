20218133b1c6b277e8a48d387eaf2cdc11a15b8d
Kacper <kacper0775@gmail.com>
2021-05-30 22:28:57 +0200

    change structure in ProjectsListElements

--- src/components/projectsList/ProjectsListElements.js
+++ src/components/projectsList/ProjectsListElements.js
@@ -12,7 +12,7 @@ export const projects = [
     {
         id: 2,
         name: "devjobs",
-        description: "devjobs umożliwia wyszukiwanie ofert pracy na stanowisko programisty, zaimplementowane filtrowanie pomaga w wyszukaniu oferty pracy, która nas interesuje. Każda oferta posiada dokładny opis jak i link, pod który należy aplikować. Opcja przypinania ofert pozwala na zachowanie ofert, które nas interesują. devjobs Posiada także tryb dnia i nocy, który zmienia kolory UI.",
+        description: "devjobs umożliwia wyszukiwanie ofert pracy na stanowisko programisty, zaimplementowane filtrowanie pomaga w wyszukaniu oferty pracy, która nas interesuje. Każda oferta posiada dokładny opis jak i link, pod który należy aplikować. Opcja przypinania pozwala na zachowanie ofert, które nas interesują. devjobs Posiada także tryb dnia i nocy, który zmienia kolory UI.",
         githubLink: 'https://github.com/gacbur/devjobs',
         liveServerLink: 'https://devjobs-one.vercel.app/',
         image: "https://user-images.githubusercontent.com/46655309/112963910-20450880-9148-11eb-8d58-8f6804e66d9e.JPG",
@@ -20,20 +20,20 @@ export const projects = [
     },
     {
         id: 3,
+        name: "Movies App",
+        description: "Movies App umożliwia wyszukiwanie filmów, każdy film zawiera: opis, galerie zdjęć, średnią ocen, aktorów, obsadę i inne informacje. Filmy można dodawać do ulubionych i w ten sposób prowadzić kolekcję ulubionych filmów. Każdy film na dole strony proponuje tytuły, które mogą się spodobać, biorąc pod uwagę obecnie przeglądany film.",
+        githubLink: 'https://github.com/gacbur/moviesapp',
+        liveServerLink: 'https://movies--app.vercel.app/',
+        image: "https://user-images.githubusercontent.com/46655309/120081103-07ab8d00-c0bc-11eb-807a-d07370d945d7.JPG",
+        icons: ['assets/dev-icons/javascript.png', 'assets/dev-icons/html.png', 'assets/dev-icons/css.png', 'assets/dev-icons/sass.png', 'assets/dev-icons/material-ui.png', 'assets/dev-icons/redux.png']
+    },
+    {
+        id: 4,
         name: "Diet tracker",
         description: "Diet tracker to aplikacja, która pomaga w prowadzeniu diety jak i śledzeniu wagi ciała, dodawanie posiłków na dany dzień służy prowadzeniu skutecznej diety poprzez śledzenie ilości i proporcji spożytych kalorii i makroskładników z posiłków. Zarówno waga jak i dane żywieniowe zapisują się, co umożliwia wrócenie do nich w każdej chwili, aplikacja zawiera również wygodne tabelki, graf i wykres.",
         githubLink: 'https://github.com/gacbur/fitness-tracker',
         liveServerLink: 'https://diet-tracker.vercel.app/',
         image: "https://user-images.githubusercontent.com/46655309/112970643-b11ee280-914e-11eb-9f6d-f703eb6fb4a7.JPG",
         icons: ['assets/dev-icons/typescript.png', 'assets/dev-icons/html.png', 'assets/dev-icons/sass.png', 'assets/dev-icons/material-ui.png', 'assets/dev-icons/redux.png']
-    },
-    {
-        id: 4,
-        name: "Movies viewer",
-        description: "Movies viewer umożliwia wyszukiwanie filmów, każdy film zawiera: opis, średnią ocen, aktorów, obsadę i inne informacje. Filmy można dodawać do ulubionych i w ten sposób prowadzić kolekcję ulubionych filmów. Każdy film na dole strony proponuje tytuły, które mogą się spodobać, biorąc pod uwagę obecnie przeglądany film.",
-        githubLink: 'https://github.com/gacbur/mern-movies-app',
-        liveServerLink: 'https://moviesviewer.vercel.app/',
-        image: "https://user-images.githubusercontent.com/46655309/112980528-4673a400-915a-11eb-9a48-0f6fc05f0b4f.JPG",
-        icons: ['assets/dev-icons/javascript.png', 'assets/dev-icons/html.png', 'assets/dev-icons/css.png', 'assets/dev-icons/material-ui.png', 'assets/dev-icons/redux.png']
     }
 ]
